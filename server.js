976de2009b91c04689f220a1594ce08d9705e6ea
Sardar Ali Murad Ali <120336967+Sardar-Ali-Murad@users.noreply.github.com>
2023-01-18 23:08:49 +0500

    Update server.js

--- server.js
+++ server.js
@@ -71,7 +71,7 @@ app.use(function(req, res, next) {
 
 app.use(cors({
   credentials: true,
-  origin: ['http://localhost:3000']
+  origin: ['http://localhost:3000','https://akbuilderpk.onrender.com']
 }))
 
 
